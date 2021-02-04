fdf254faaec5f7e4fe63dc8e1a449abe0b89b09b
지호 김 <jjiho0126@gmail.com>
2021-02-04 14:03:28 +0900

    디자인변경

--- Calculate/Form1.Designer.cs
+++ Calculate/Form1.Designer.cs
@@ -48,184 +48,291 @@ namespace Calculate
             this.btn_point = new System.Windows.Forms.Button();
             this.btn_0 = new System.Windows.Forms.Button();
             this.btn_00 = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // result
             // 
-            this.result.Location = new System.Drawing.Point(12, 12);
+            this.result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.result.Enabled = false;
+            this.result.Font = new System.Drawing.Font("넥슨Lv2고딕", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.result.Location = new System.Drawing.Point(3, 9);
+            this.result.Multiline = true;
             this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(312, 25);
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(304, 46);
             this.result.TabIndex = 0;
+            this.result.Text = "0";
+            this.result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // btn_1
             // 
-            this.btn_1.Location = new System.Drawing.Point(12, 106);
+            this.btn_1.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_1.FlatAppearance.BorderSize = 0;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_1.Location = new System.Drawing.Point(10, 237);
             this.btn_1.Name = "btn_1";
             this.btn_1.Size = new System.Drawing.Size(58, 56);
             this.btn_1.TabIndex = 1;
             this.btn_1.Text = "1";
-            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.UseVisualStyleBackColor = false;
             // 
             // btn_2
             // 
-            this.btn_2.Location = new System.Drawing.Point(76, 106);
+            this.btn_2.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_2.FlatAppearance.BorderSize = 0;
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_2.Location = new System.Drawing.Point(74, 237);
             this.btn_2.Name = "btn_2";
             this.btn_2.Size = new System.Drawing.Size(58, 56);
             this.btn_2.TabIndex = 2;
             this.btn_2.Text = "2";
-            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.UseVisualStyleBackColor = false;
             // 
             // btn_3
             // 
-            this.btn_3.Location = new System.Drawing.Point(140, 106);
+            this.btn_3.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_3.FlatAppearance.BorderSize = 0;
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_3.Location = new System.Drawing.Point(138, 237);
             this.btn_3.Name = "btn_3";
             this.btn_3.Size = new System.Drawing.Size(58, 56);
             this.btn_3.TabIndex = 3;
             this.btn_3.Text = "3";
-            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.UseVisualStyleBackColor = false;
             // 
             // btn_minus
             // 
-            this.btn_minus.Location = new System.Drawing.Point(76, 43);
+            this.btn_minus.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_minus.FlatAppearance.BorderSize = 0;
+            this.btn_minus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_minus.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_minus.Location = new System.Drawing.Point(202, 237);
             this.btn_minus.Name = "btn_minus";
             this.btn_minus.Size = new System.Drawing.Size(58, 56);
             this.btn_minus.TabIndex = 4;
             this.btn_minus.Text = "-";
-            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.UseVisualStyleBackColor = false;
             // 
             // btn_4
             // 
-            this.btn_4.Location = new System.Drawing.Point(12, 168);
+            this.btn_4.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_4.FlatAppearance.BorderSize = 0;
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_4.Location = new System.Drawing.Point(10, 175);
             this.btn_4.Name = "btn_4";
             this.btn_4.Size = new System.Drawing.Size(58, 56);
             this.btn_4.TabIndex = 5;
             this.btn_4.Text = "4";
-            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.UseVisualStyleBackColor = false;
             // 
             // btn_6
             // 
-            this.btn_6.Location = new System.Drawing.Point(140, 168);
+            this.btn_6.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_6.FlatAppearance.BorderSize = 0;
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_6.Location = new System.Drawing.Point(138, 175);
             this.btn_6.Name = "btn_6";
             this.btn_6.Size = new System.Drawing.Size(58, 56);
             this.btn_6.TabIndex = 6;
             this.btn_6.Text = "6";
-            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.UseVisualStyleBackColor = false;
             // 
             // btn_5
             // 
-            this.btn_5.Location = new System.Drawing.Point(76, 168);
+            this.btn_5.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_5.FlatAppearance.BorderSize = 0;
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_5.Location = new System.Drawing.Point(74, 175);
             this.btn_5.Name = "btn_5";
             this.btn_5.Size = new System.Drawing.Size(58, 56);
             this.btn_5.TabIndex = 7;
             this.btn_5.Text = "5";
-            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.UseVisualStyleBackColor = false;
             // 
             // btn_multiple
             // 
-            this.btn_multiple.Location = new System.Drawing.Point(140, 43);
+            this.btn_multiple.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_multiple.FlatAppearance.BorderSize = 0;
+            this.btn_multiple.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_multiple.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_multiple.Location = new System.Drawing.Point(202, 175);
             this.btn_multiple.Name = "btn_multiple";
             this.btn_multiple.Size = new System.Drawing.Size(58, 56);
             this.btn_multiple.TabIndex = 8;
             this.btn_multiple.Text = "x";
-            this.btn_multiple.UseVisualStyleBackColor = true;
+            this.btn_multiple.UseVisualStyleBackColor = false;
             // 
             // btn_7
             // 
-            this.btn_7.Location = new System.Drawing.Point(12, 230);
+            this.btn_7.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_7.FlatAppearance.BorderSize = 0;
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_7.Location = new System.Drawing.Point(10, 112);
             this.btn_7.Name = "btn_7";
             this.btn_7.Size = new System.Drawing.Size(58, 56);
             this.btn_7.TabIndex = 9;
             this.btn_7.Text = "7";
-            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.UseVisualStyleBackColor = false;
             // 
             // btn_8
             // 
-            this.btn_8.Location = new System.Drawing.Point(76, 230);
+            this.btn_8.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_8.FlatAppearance.BorderSize = 0;
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_8.Location = new System.Drawing.Point(74, 112);
             this.btn_8.Name = "btn_8";
             this.btn_8.Size = new System.Drawing.Size(58, 56);
             this.btn_8.TabIndex = 10;
             this.btn_8.Text = "8";
-            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.UseVisualStyleBackColor = false;
             // 
             // btn_9
             // 
-            this.btn_9.Location = new System.Drawing.Point(140, 230);
+            this.btn_9.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_9.FlatAppearance.BorderSize = 0;
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_9.Location = new System.Drawing.Point(138, 112);
             this.btn_9.Name = "btn_9";
             this.btn_9.Size = new System.Drawing.Size(58, 56);
             this.btn_9.TabIndex = 11;
             this.btn_9.Text = "9";
-            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.UseVisualStyleBackColor = false;
             // 
             // btn_divide
             // 
-            this.btn_divide.Location = new System.Drawing.Point(204, 44);
+            this.btn_divide.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_divide.FlatAppearance.BorderSize = 0;
+            this.btn_divide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_divide.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_divide.Location = new System.Drawing.Point(202, 112);
             this.btn_divide.Name = "btn_divide";
             this.btn_divide.Size = new System.Drawing.Size(58, 56);
             this.btn_divide.TabIndex = 12;
             this.btn_divide.Text = "/";
-            this.btn_divide.UseVisualStyleBackColor = true;
+            this.btn_divide.UseVisualStyleBackColor = false;
             // 
             // btn_plus
             // 
-            this.btn_plus.Location = new System.Drawing.Point(12, 44);
+            this.btn_plus.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_plus.FlatAppearance.BorderSize = 0;
+            this.btn_plus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_plus.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_plus.Location = new System.Drawing.Point(202, 299);
             this.btn_plus.Name = "btn_plus";
             this.btn_plus.Size = new System.Drawing.Size(58, 56);
             this.btn_plus.TabIndex = 16;
             this.btn_plus.Text = "+";
-            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.UseVisualStyleBackColor = false;
             // 
             // button14
             // 
-            this.button14.Location = new System.Drawing.Point(266, 44);
+            this.button14.BackColor = System.Drawing.SystemColors.Control;
+            this.button14.FlatAppearance.BorderSize = 0;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button14.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button14.Location = new System.Drawing.Point(268, 113);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(58, 118);
             this.button14.TabIndex = 15;
             this.button14.Text = "CE";
-            this.button14.UseVisualStyleBackColor = true;
+            this.button14.UseVisualStyleBackColor = false;
             this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // btn_result
             // 
-            this.btn_result.Location = new System.Drawing.Point(266, 168);
+            this.btn_result.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_result.FlatAppearance.BorderSize = 0;
+            this.btn_result.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_result.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_result.Location = new System.Drawing.Point(268, 237);
             this.btn_result.Name = "btn_result";
             this.btn_result.Size = new System.Drawing.Size(58, 118);
             this.btn_result.TabIndex = 17;
             this.btn_result.Text = "=";
-            this.btn_result.UseVisualStyleBackColor = true;
+            this.btn_result.UseVisualStyleBackColor = false;
             this.btn_result.Click += new System.EventHandler(this.btn_result_Click);
             // 
             // btn_point
             // 
-            this.btn_point.Location = new System.Drawing.Point(204, 230);
+            this.btn_point.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_point.FlatAppearance.BorderSize = 0;
+            this.btn_point.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_point.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_point.Location = new System.Drawing.Point(138, 299);
             this.btn_point.Name = "btn_point";
             this.btn_point.Size = new System.Drawing.Size(58, 56);
             this.btn_point.TabIndex = 18;
             this.btn_point.Text = ".";
-            this.btn_point.UseVisualStyleBackColor = true;
+            this.btn_point.UseVisualStyleBackColor = false;
             // 
             // btn_0
             // 
-            this.btn_0.Location = new System.Drawing.Point(204, 106);
+            this.btn_0.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_0.FlatAppearance.BorderSize = 0;
+            this.btn_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_0.Location = new System.Drawing.Point(74, 299);
             this.btn_0.Name = "btn_0";
             this.btn_0.Size = new System.Drawing.Size(58, 56);
             this.btn_0.TabIndex = 19;
             this.btn_0.Text = "0";
-            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.UseVisualStyleBackColor = false;
             // 
             // btn_00
             // 
-            this.btn_00.Location = new System.Drawing.Point(204, 168);
+            this.btn_00.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_00.FlatAppearance.BorderSize = 0;
+            this.btn_00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_00.Font = new System.Drawing.Font("넥슨Lv2고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_00.Location = new System.Drawing.Point(10, 299);
             this.btn_00.Name = "btn_00";
             this.btn_00.Size = new System.Drawing.Size(58, 56);
             this.btn_00.TabIndex = 20;
             this.btn_00.Text = "00";
-            this.btn_00.UseVisualStyleBackColor = true;
+            this.btn_00.UseVisualStyleBackColor = false;
+            // 
+            // prev
+            // 
+            this.prev.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.prev.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.prev.Font = new System.Drawing.Font("넥슨Lv2고딕 Light", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.prev.Location = new System.Drawing.Point(12, 9);
+            this.prev.Multiline = true;
+            this.prev.Name = "prev";
+            this.prev.ReadOnly = true;
+            this.prev.Size = new System.Drawing.Size(314, 40);
+            this.prev.TabIndex = 21;
+            this.prev.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.result);
+            this.panel1.Location = new System.Drawing.Point(12, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(314, 60);
+            this.panel1.TabIndex = 22;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 300);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(336, 370);
+            this.Controls.Add(this.prev);
             this.Controls.Add(this.btn_00);
             this.Controls.Add(this.btn_0);
             this.Controls.Add(this.btn_point);
@@ -244,10 +351,12 @@ namespace Calculate
             this.Controls.Add(this.btn_3);
             this.Controls.Add(this.btn_2);
             this.Controls.Add(this.btn_1);
-            this.Controls.Add(this.result);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,6 +383,8 @@ namespace Calculate
         private System.Windows.Forms.Button btn_point;
         private System.Windows.Forms.Button btn_0;
         private System.Windows.Forms.Button btn_00;
+        private System.Windows.Forms.TextBox prev;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Calculate/Form1.cs
+++ Calculate/Form1.cs
@@ -13,9 +13,9 @@ namespace Calculate
 {
     public partial class Form1 : Form
     {
-        Double num1;
-        Double num2;
-        String temp;
+        private Double num1;
+        private Double num2;
+        private String temp;
 
         public Form1()
         {
@@ -51,6 +51,7 @@ namespace Calculate
 
             // 버튼 클릭시 해당 버튼값 가져와서 텍스트창에 보여주기
             result.Text += btn.Text;
+            
             // 만약 앞에 02 / 066이라면 0 없애버리기
             if(result.Text.Length > 1 && result.Text.StartsWith("0"))
             {
@@ -64,20 +65,34 @@ namespace Calculate
             /// 기호가 뭔지 저장
             Button btn = sender as Button;
             temp = btn.Text;
-            // 현재 텍스트창에 암것도 없으면 return
-            if (result.Text == "") return;
+            // 현재 텍스트창 빈칸
+            // - 누르면 부호 - 붙여주기
+            // 나머지 기호 누르면 그냥 return
+            if (result.Text == "")
+            {
+                if (temp.Equals("-"))
+                {
+                    result.Text += "-";
+                }
+                return;
+            }
             // 현재 텍스트창에 있는 값 저장
             //num1 = Double.Parse(result.Text);
             Double.TryParse(result.Text, out num1);
             Console.WriteLine("num1 = {0}", num1);
-            result.Text = null;
-
+            // 숫자들 보여주기
+            prev.Text += num1;
+            prev.Text += temp;
+            result.Text = "";
         }
 
         private void btn_result_Click(object sender, EventArgs e)
         {
             // = 버튼 눌렀을때의 결과
             num2 = Double.Parse(result.Text);
+            // prev에 = 
+            prev.Text += num2;
+            prev.Text += "=";
             Calculator cal = new Calculator();
 
             // 그리고서 temp가 무엇인지 확인하기
@@ -108,11 +123,13 @@ namespace Calculate
         private void cal_clear()
         {
             // CE 버튼 클릭시 텍스트박스 초기화
-            result.Text = "";
+            result.Text = "0";
+            prev.Text = "";
             num1 = 0;
             num2 = 0;
             temp = null;
         }
+
     }
        
 }
